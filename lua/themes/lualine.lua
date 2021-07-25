7b85bf0ea79cce843d5deab3ad3be4a8ed903a45
yeyee2901 <yhxzen2901@gmail.com>
2021-07-25 16:34:54 +0700

    Add diagnostic & git info to lualine.

--- lua/themes/lualine.lua
+++ lua/themes/lualine.lua
@@ -10,15 +10,40 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    --lualine_d = {'diff'},    -- BUG!
+    lualine_b = {
+      {'branch'},
+      {
+        'diff',
+        colored = true,
+        color_added = '#16f70a',
+        color_modified = '#dff705',
+        color_removed = '#f71b02',
+        symbols = {added = '+', modified = '~', removed = '-'}
+      },
+    },
+    lualine_c = {
+      {
+        'filename',
+        path = 1    -- 1 = relative path
+      },
+      {
+        'diagnostics',
+        sources = {'nvim_lsp'},
+        symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
+        color_error = '#f71b02',
+        color_warn = '#dff705',
+        color_info = '#16f70a',
+        color_hint = '#05d8f0',
+      },
+    },
     lualine_x = {
       {
         'filetype',
         colored = true
       },
-      'encoding'
+      {
+        'encoding'
+      },
     },
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -26,15 +51,26 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+      { 'filename' },
+    },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
   },
+
   tabline = {
     lualine_a = {},
     lualine_b = {'branch'},
-    lualine_c = {'filename',{ 'filetype', colored = true }},
+    lualine_c = {
+      {
+        'filename',
+      },
+      {
+        'filetype',
+        colored = true,
+      }
+    },
   },
 
   extensions = {}
